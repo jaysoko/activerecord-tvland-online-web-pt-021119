4e39c34d8e299ccaec268be889f0b0565c14422e
Jay Sokolowski <jaysoko@gmail.com>
2019-05-13 00:09:41 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,8 +4,7 @@ has_many :characters
 has_many :shows, through: :characters
 
 def full_name
-  full_name = self.first_name self.last_name
-  full_name
+  "#{self.first_name} #{self.last_name}"
 end
 
 end
