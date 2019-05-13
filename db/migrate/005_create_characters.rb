f4c216ccb5cf820025934149f49d00a978e29562
Jay Sokolowski <jaysoko@gmail.com>
2019-05-13 00:30:30 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,7 +3,7 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
     create_table :characters do |t|
       #primary key of :id is created for us!
       t.string :name
-      t.string :catchphrase
+      t.string :say_that_thing_you_say
       t.integer :show_id
       t.integer :actor_id
     end
